a14ce44bedb1af06a47e99ba1061ebf35b2ccabe
Prernakumari29 <prernajaiswal646@gmail.com>
2026-04-28 23:04:18 +0530

    modify : UI/UX of IntegrateWithRedux file

--- IntegrateWithRedux/src/Authen/Register.jsx
+++ IntegrateWithRedux/src/Authen/Register.jsx
@@ -22,9 +22,9 @@ const Register = ({ setToggle }) => {
   }
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-black via-gray-500 to-gray-400">
+    <div className="min-h-screen flex items-center justify-center ">
 
-      <div className="bg-white/20 backdrop-blur-lg p-8 rounded-2xl shadow-2xl w-96 text-white">
+      <div className="bg-white/20 backdrop-blur-lg p-8 rounded-2xl shadow-2xl w-96 text-black">
 
         <h1 className="text-3xl font-bold text-center mb-6">Register</h1>
 
@@ -36,7 +36,7 @@ const Register = ({ setToggle }) => {
             value={formData.name || ""}
             type="text"
             placeholder='Enter your name'
-            className="w-full px-4 py-2 rounded-lg bg-white/30 placeholder-white outline-none focus:ring-2 focus:ring-white"
+            className="w-full px-4 py-2 rounded-lg bg-black/50 placeholder-white outline-none focus:ring-2"
           />
 
           <input
@@ -45,7 +45,7 @@ const Register = ({ setToggle }) => {
             value={formData.email || ""}
             type="email"
             placeholder='Enter your email'
-            className="w-full px-4 py-2 rounded-lg bg-white/30 placeholder-white outline-none focus:ring-2 focus:ring-white"
+            className="w-full px-4 py-2 rounded-lg bg-black/50 placeholder-white outline-none focus:ring-2 focus:ring-white"
           />
 
           <input
@@ -54,7 +54,7 @@ const Register = ({ setToggle }) => {
             value={formData.password || ""}
             type="password"
             placeholder='Enter your password'
-            className="w-full px-4 py-2 rounded-lg bg-white/30 placeholder-white outline-none focus:ring-2 focus:ring-white"
+            className="w-full px-4 py-2 rounded-lg bg-black/50 placeholder-white outline-none focus:ring-2 focus:ring-white"
           />
 
           <input
@@ -63,7 +63,7 @@ const Register = ({ setToggle }) => {
             value={formData.number || ""}
             type="text"
             placeholder='Enter your number'
-            className="w-full px-4 py-2 rounded-lg bg-white/30 placeholder-white outline-none focus:ring-2 focus:ring-white"
+            className="w-full px-4 py-2 rounded-lg bg-black/50 placeholder-white outline-none focus:ring-2 focus:ring-white"
           />
 
           <button
